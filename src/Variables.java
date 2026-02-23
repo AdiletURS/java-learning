2430a6ef8593e5a383b980c734aa08673090d455
Adilet Tursunbekov <adileturs@gmai.com>
2026-02-23 08:23:05 +0600

    feat: add variables demo

---
+++ src/Variables.java
@@ -0,0 +1,13 @@
+public class Variables {
+    public static void main(String[] args){
+        int age = 22;
+        double price = 66.66;
+        boolean isStudent = true;
+        String name = "Adilet";
+        String city = "Bishkek";
+        char grade = 'A';
+
+
+        System.out.println("Имя: " + name + "\nЛет:  " + age + "\nГород: "  + city + "\nЦена: "  + price + "\nСтудент: " + isStudent + "\nОценка: " + grade);
+    }
+}
